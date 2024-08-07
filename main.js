834ff7a0c57bb1b9dba627b3ffcbfd5bd12eef1a
User-Takhirov <tohirovfayruz@gmail.com>
2024-08-08 00:08:19 +0500

    function

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-let a =12;
+// let a = 121;
+// console.log(a);
+
+// console object
+// log esa method
+// console degan objectni log degan  methodi
+
+// function decloration
+// function jamshid() {
+//   console.log("jamshid");
+// }
+// console.log(lorem);
+
+// funksiyalar qachonki chaqirilgan holatda ishlidi
+// agar funksiyani console log bilan chaqirilsa u funkisiyani toliq chiqarib beradi; bu notogri method hisoblanadi ;
+// funkisyani ya'ni jamshidni ozini bracket bilan chaqirib va scope ochib slash qoyih kerak sababi js single thread hisoblandi;
+// decloration functionni yaratishdan oldin ham chaqirsa boladi; bunaqa ishlatishni tavsiya qilinmidi;
+
+// jamshid();//////////////////////////////
+
+// console.log("javohir");
+
+
+
+
+// funkiyaga argument berish
+
+// let a = 90;
+// function jamshid(num) {
+//   console.log(num + 90);
+// }  //tepadigi qismi parametr qeyiladi
+
+
+// jamshid(a);  //pasda berilgan qism argument deyiladi 
+
+
+
+
+
+
+// undefined = ozgaruvchi elon qilinganda lekin qiymat berilmaganda undefined chiqaradi
+// dead zone =variable elon qilingan qatordan tepadagi qator dead zone deyiladi
+// dasturlar 3 turga bolinadi chiziqli funksiya yonaltirilgan  va obyektga yonatilgan
+// funksiya 1 ta kodni qayta qayta ishlatish uchun ishlatiladi mixinga oxhsab
